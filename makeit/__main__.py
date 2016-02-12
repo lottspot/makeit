82f888c607005cf6ae81583b4ff86a5241b0732e
James Lott <james@lottspot.com>
2016-02-11 21:04:05 -0500

    Implement main routine

---
+++ makeit/__main__.py
@@ -0,0 +1,19 @@
+import sys
+from configparser import SafeConfigParser
+from doit.doit_cmd import DoitMain
+from .loader import MakeItLoader
+
+makeit_cfg = 'makeit.cfg'
+
+def cfg_to_dict(cfg):
+    d = {}
+    for section in cfg.sections():
+        d[section] = dict(cfg[section])
+    return d
+
+if __name__ == '__main__':
+    cfg = SafeConfigParser()
+    if makeit_cfg not in cfg.read(makeit_cfg):
+        sys.exit('Failed to load %s' % makeit_cfg)
+    as_dict = cfg_to_dict(cfg)
+    sys.exit(DoitMain(MakeItLoader(as_dict)).run(sys.argv[1:]))
