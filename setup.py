ec27d1d78dd893701b280971502d43932a90740a
James Lott <james@lottspot.com>
2016-02-11 21:58:15 -0500

    Prevent exceptions for missing development module

--- setup.py
+++ setup.py
@@ -1,35 +1,48 @@
 import sys, os
-import coverage
 from StringIO import StringIO
 from setuptools import setup, Command
 from tests.__main__ import main as testmain
 
-class Coverage(Command):
-    description  = 'Calculate code coverage of unit tests'
-    user_options = []
-    def run(self):
-        cover  = coverage.Coverage()
-        buffer = StringIO()
-        cover.start()
-        testmain()
-        cover.stop()
-        cover.save()
-        cover.html_report()
-        cover.report(file=buffer)
-        buffer.seek(0)
-        for line in buffer.readlines():
-            columns = line.split()
-            # Exclude coverage reports on external modules
-            if os.path.abspath(columns[0]).startswith(sys.prefix):
-                continue
-            # The total is not accurate since we're filtering out entries
-            if columns[0] == 'TOTAL':
-                continue
-            sys.stdout.write(line)
-    def initialize_options(self):
-        pass
-    def finalize_options(self):
-        pass
+try:
+    import coverage
+except ImportError:
+    coverage = None
+
+if coverage:
+    class Coverage(Command):
+        description  = 'Calculate code coverage of unit tests'
+        user_options = []
+        def run(self):
+            cover  = coverage.Coverage()
+            buffer = StringIO()
+            cover.start()
+            testmain()
+            cover.stop()
+            cover.save()
+            cover.html_report()
+            cover.report(file=buffer)
+            buffer.seek(0)
+            for line in buffer.readlines():
+                columns = line.split()
+                # Exclude coverage reports on external modules
+                if os.path.abspath(columns[0]).startswith(sys.prefix):
+                    continue
+                # The total is not accurate since we're filtering out entries
+                if columns[0] == 'TOTAL':
+                    continue
+                sys.stdout.write(line)
+        def initialize_options(self):
+            pass
+        def finalize_options(self):
+            pass
+else:
+    class Coverage(Command):
+        def run(self):
+            pass
+        def initialize_options(self):
+            pass
+        def finalize_options(self):
+            pass
 
 setup(
     name='makeit',
